9e2dc6d96950e0c3302c81162cf95c144e2777ad
Liu Yue <hangxingliu@gmail.com>
2019-03-06 18:16:30 +0800

    update release-check.sh

--- release-check.sh
+++ release-check.sh
@@ -28,13 +28,16 @@ has_change_log_title() {
 [[ -z "$(has_change_log_title CHANGELOG.md)" ]] && throw "CHANGELOG.md doesn't contain \"$change_log_title\"";
 
 
-# about git
-if [[ -n `git status --short` ]]; then git status --short; throw "there some files are not commited!"; fi
-[[ "$(git push --dry-run 2>/dev/stdout)" == "Everything up-to-date" ]] || throw "there some commits are not pushed!";
-
 echo "packing up archive files ...";
 npm pack || throw "npm pack failed!";
 vsce package || throw "vsce package failed!";
+
+echo "the target archive files ...";
+ls *.{tgz,vsix} -alh;
+
+# about git
+if [[ -n `git status --short` ]]; then git status --short; throw "there some files are not commited!"; fi
+[[ "$(git push --dry-run 2>/dev/stdout)" == "Everything up-to-date" ]] || throw "there some commits are not pushed!";
 
 echo "============================";
 echo "Release Version: $release_version";
